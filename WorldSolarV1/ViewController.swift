b2ede496cdd17236cdba7107edee197938b95441
CodingApps <codingkit@gmail.com>
2018-10-09 09:50:14 -0700

    Initial Commit

---
+++ WorldSolarV1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldSolarV1
+//
+//  Created by Rick Mc on 10/9/18.
+//  Copyright © 2018 Rick Mc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
